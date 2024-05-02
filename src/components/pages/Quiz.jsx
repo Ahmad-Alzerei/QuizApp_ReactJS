86725874968d2eb8b0e1bbb8b2d16e0762dbb524
Ahmad Alzerei <ahmadzerie40@gmail.com>
2024-05-02 17:35:04 +0300

    the third commit now the application is working using fake data called constants and the application need more modificatin like adding a timer to  it after this commit

--- src/components/pages/Quiz.jsx
+++ src/components/pages/Quiz.jsx
@@ -2,12 +2,18 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line react/prop-types
 import { useState } from "react";
+import { resultInitialState } from "../../data/constants";
 
 const Quiz = ({ questions }) => {
+  //States
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answerIdx, setAnswerIdx] = useState(null);
   const [answer, setAnswer] = useState(null);
   const { question, choices, correctAnswer } = questions[currentQuestion];
+  const [result, setResult] = useState(resultInitialState);
+  const [showResult, setShowResult] = useState(false);
+
+  //Handlers
   const onAnswerClick = (answer, index) => {
     setAnswerIdx(index);
     if (answer === correctAnswer) {
@@ -17,22 +23,76 @@ const Quiz = ({ questions }) => {
     }
   };
 
+  const onClickNext = () => {
+    setAnswerIdx(null);
+    setResult((prev) =>
+      answer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }
+    );
+    console.log(result);
+    if (currentQuestion !== questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+    } else {
+      setCurrentQuestion(0);
+      setShowResult(true);
+    }
+  };
+  const onTryAgain = () => {
+    setResult(resultInitialState);
+    setShowResult(false);
+  };
+
   return (
     <div className="quiz-container">
-      <>
-        <span className="active-question-no">{currentQuestion + 1}</span>
-        <span className="total-question">/{questions.length}</span>
-        <h2>{question}</h2>
-        <ul>
-          {choices.map((answer, index) => {
-            <li onClick={() => onAnswerClick(answer, index)} key={answer} className="">
-              {answer}
-            </li>;
-          })}
-        </ul>
-      </>
+      {!showResult ? (
+        <>
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-question">/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((answer, index) => (
+              <li
+                onClick={() => onAnswerClick(answer, index)}
+                key={answer}
+                className={answerIdx === index ? "selected-answer" : null}
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className="footer">
+            <button onClick={onClickNext} disabled={answerIdx === null}>
+              {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Questions: <span>{questions.length}</span>
+          </p>
+          <p>
+            Total Score: <span>{result.score}</span>
+          </p>
+          <p>
+            Correct Answers: <span>{result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers: <span>{result.wrongAnswers}</span>
+          </p>
+          <button onClick={onTryAgain}>Try again</button>
+        </div>
+      )}
     </div>
   );
 };
-
 export default Quiz;
